93bb5863e88e3b9300586d99184dca9d1bb06250
Tal Bronfer <tal.bronfer@safebreach.com>
2018-08-07 13:41:25 +0300

    updated test descriptions

--- .test/App.test.js
+++ .test/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import assert from 'assert';
 import App from '../src/App';
 import Letters from '../src/components/Letters';
 import Letter from '../src/components/Letter';
@@ -7,21 +8,28 @@ import Score from '../src/components/Score';
 import renderer from 'react-test-renderer';
 import { mount, render, shallow, configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { wrap } from 'module';
 
 configure({ adapter: new Adapter() });
 
-it('renders without crashing', () => {
+// expect.extend({
+//     toEqualWithMessage(received, validator, message) {
+//         if (validator.received == )
+//     }
+// })
+
+it('Application should render without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
-it('should have one Letters component', () => {
+it('App should have one Letters component in root', () => {
     const wrapper = shallow(<App />);
-    expect(wrapper.find(Letters)).toHaveLength(1);
+    expect(wrapper.find(Letters)).toHaveLength(2); // there must be exactly one Letters component
 });
 
-it('should have one Letter component under Letters', () => {
+it('App should have one Letter component under Letters', () => {
     const wrapper = shallow(<Letters />);
     expect(wrapper.find(Letter)).toHaveLength(1);
-})
+});
---
+++ test-runner.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+var jest = require('jest-cli');
+const util = require('util');
+const _ = require('lodash');
+const exec = util.promisify(require('child_process').exec);
+const fs = require('fs');
+
+var args = process.argv.splice(process.execArgv.length + 2);
+
+runTest()
+    .then(() => {
+    
+    })
+    .catch((err) => {
+        console.log("Internal execution error: " + err);
+    });
+
+async function runTest(testNamePattern) {
+    try {
+        await exec('npm test');
+    }
+
+    catch(e) { 
+        console.log("General error. Please contact the instructor team." + e);
+    }
+   
+    var results = JSON.parse(fs.readFileSync('./test-results.json'));
+    // No failed tests - pass the exercise
+    if (results.numFailedTests ===  0) {
+        console.log("Well done!");
+        process.exit(0);
+    }
+
+    else {
+        var failureMessage = "";
+        results.testResults[0].testResults.forEach((t) => {
+            if (t.status == 'failed') {
+                failureMessage = failureMessage + t.fullName + "\n";
+            }
+        });
+        
+        console.log("Oops, you have a problem in there! \n" + failureMessage);
+        process.exit(1);
+    }
+
+}
+
+exports.runTest = runTest;
