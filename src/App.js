c5dbed71a9950483040a0248a13b9a60fbc57fe1
Tal Bronfer <tal.bronfer@safebreach.com>
2018-08-09 18:53:41 +0300

    updating branch

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-import Letters from './components/Letters';
-import Letter from './components/Letter';
-import Score from './components/Score';
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        <Score />
-        <Letters />
-      </div>
-      
-    );
+    return ("Nothing here yet");
   }
 }
 
