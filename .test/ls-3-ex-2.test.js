1f053150c946cb77348e8f199281d054f77bf627
Tal Bronfer <tal.bronfer@safebreach.com>
2018-08-16 15:54:48 +0300

    Added tests for exercise 2

--- .test/ls-3-ex-2.test.js
+++ .test/ls-3-ex-2.test.js
@@ -18,29 +18,6 @@ it('Application should render without crashing', () => {
   ReactDOM.unmountComponentAtNode(div);
 });
 
-it('Selecting a correct letter should increase score by 5', () => {
-    const word = 'heythere';
-    const wrapper = mount(<App />);
-    wrapper.setState({ word: word });
-    wrapper.setState({ score: 0 });
-    wrapper.update();
-    let allLetters = wrapper.find(Letters).find("span");
-    let correctLetterWrapper = allLetters.filterWhere((l) => l.text().toLowerCase() ==='h');
-    correctLetterWrapper.first().simulate('click');
-    expect(wrapper.state('score')).toEqual(5);
-});
-
-it('Selecting an incorrect letter should decrease score by 20', () => {
-    const word = "someword";
-    const wrapper = mount(<App />);
-    wrapper.setState({ word: word });
-    wrapper.setState({ score: 50 });
-    wrapper.update();
-    let allLetters = wrapper.find(Letters).find("span");
-    let correctLetterWrapper = allLetters.filterWhere((l) => l.text().toLowerCase() === 'z');
-    correctLetterWrapper.first().simulate('click');
-    expect(wrapper.state('score')).toEqual(30);
- });
 
  it('When the user has more than 80 points, the Score div should have the "high-score" class', () => {
     const expectedClassName = 'high-score'; 
@@ -49,6 +26,24 @@ it('Selecting an incorrect letter should decrease score by 20', () => {
     wrapper.update();
     let scoreDiv = wrapper.find(Score).find('div').first();
     expect(scoreDiv.hasClass(expectedClassName));
+ });
+
+ it ('When the user has between 50 and 80 points, the Score div should have the "medium-score" class', () => {
+     const expectedClassName = 'medium-score';
+     const wrapper = mount(<App />);
+     wrapper.setState({ score: 60 });
+     wrapper.update();
+     let scoreDiv = wrapper.find(Score).find('div').first();
+     expect(scoreDiv.hasClass(expectedClassName));
+ })
+
+ it('When the user has less than 50 points, the Score div should have the "low-score" class', () => {
+     const expectedClassName = 'low-score';
+     const wrapper = mount(<App />);
+     wrapper.setState({ score: 10 });
+     wrapper.update(); 
+     let scoreDiv = wrapper.find(Score).find('div').first();
+     expect(scoreDiv.hasClass(expectedClassName));
  });
 
 
--- src/components/Score.js
+++ src/components/Score.js
@@ -4,8 +4,22 @@ import React, { Component } from 'react';
 class Score extends Component {
   render() {
     return (
-      <div>Score: {this.props.score}</div>
+      <div className={ this.getScoreClassName() }>Score: {this.props.score}</div>
     );
+  }
+
+  getScoreClassName() {
+    if (this.props.score >= 80) {
+      return "high-score";
+    }
+    
+    else if (this.props.score < 80 && this.props.score >= 50) {
+      return "medium-score";
+    }
+
+    else {
+      return "low-score";
+    }
   }
 }
 
