26049329576c4f569cf1ce512d3e19b0ff472f48
Tal Bronfer <tal.bronfer@safebreach.com>
2018-10-14 11:03:16 +0300

    moving tests outside of secure

--- .guides/.test/ex-1.test.js
+++ .guides/.test/ex-1.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import assert from 'assert';
-import App from '../../../src/App';
-import Letters from '../../../src/components/Letters';
-import Letter from '../../../src/components/Letter';
-import Score from '../../../src/components/Score';
+import App from '../../src/App';
+import Letters from '../../src/components/Letters';
+import Letter from '../../src/components/Letter';
+import Score from '../../src/components/Score';
 import renderer from 'react-test-renderer';
 import { mount, render, shallow, configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
