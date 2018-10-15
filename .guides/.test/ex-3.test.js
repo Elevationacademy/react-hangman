301e8275cb81362d85502fde3c2e1d95dce3a73d
JonaCodes <jonathanfarache@gmail.com>
2018-10-15 16:58:22 +0300

    change tests to be smaller, don't check click simulation

--- .guides/.test/ex-3.test.js
+++ .guides/.test/ex-3.test.js
@@ -13,44 +13,33 @@ import { wrap } from 'module';
 
 configure({ adapter: new Adapter() });
 
-// it('Application should render without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
+it('Application should render without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
 
 
 it('The game should display a Congratulations div with the class "success-message" only if the word is guessed correctly', (done) => {
-    const word = "HEY";
-    const correctLetters = ['h', 'e', 'y'];
     const wrapper = mount(<App />);
     expect(wrapper.find(".success-message")).toHaveLength(0);
-    wrapper.setState({ word: word });
-    let allLetters = wrapper.find(Letters).find("span");
-    correctLetters.map((cl) => {
-        let l = allLetters.filterWhere((al) => al.text().toLowerCase() == cl).first();
-        l.simulate('click');
-    });
+    
+    const wordLetters = wrapper.state('word').split("")
+    let letterStatus = {...wrapper.state('letterStatus')}
+    wordLetters.forEach(wl => letterStatus[wl] = true)
+    wrapper.setState({letterStatus})
     wrapper.update();
+
     expect(wrapper.find('.success-message')).toHaveLength(1);
-    console.log(wrapper.html());
     done();
 
 });
 
-// // TODO: Check about -10 bug
 it('The game should display a Game Over div with the class "game-over" if the Score is below 0', () => {
-    const word = "HEY";
-    const incorrectLetters = ['a', 'b', 'c', 'd', 'f', 'g'];
     const wrapper = mount(<App />);
-    expect(wrapper.find(".error-message")).toHaveLength(0);
-    wrapper.setState({ word: word });
-    let allLetters = wrapper.find(Letters).find("span");
-    incorrectLetters.forEach((l) => {
-       let currentLetter = allLetters.filterWhere((al) => al.text().toLowerCase() == l).first(); 
-       currentLetter.simulate('click');
-    });
+    expect(wrapper.find(".game-over")).toHaveLength(0);
+    wrapper.setState({score: 0})
     wrapper.update();
-    expect(wrapper.find('.error-message')).toHaveLength(1);
+    expect(wrapper.find('.game-over')).toHaveLength(1);
 });
 
